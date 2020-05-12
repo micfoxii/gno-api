e23473bf28c68fe93284c16b1d8cd1a26c2a2de8
github username <github email address>
2020-05-12 18:50:54 -0400

    beginning of validations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,5 +2,7 @@ class Destination < ApplicationRecord
 
     has_many :reviews
     belongs_to :location
-    
+
+    validates :name, presence: true
+    validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
     has_many :reviews
-    
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness: true
+    validates :password_digest, presence: true
 end
