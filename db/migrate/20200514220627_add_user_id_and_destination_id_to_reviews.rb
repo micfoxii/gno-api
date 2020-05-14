c7d583f903bf2754eca36874127c674e6dbcf163
github username <github email address>
2020-05-14 18:11:55 -0400

    fixed add_column migration

--- db/migrate/20200514220627_add_user_id_and_destination_id_to_reviews.rb
+++ db/migrate/20200514220627_add_user_id_and_destination_id_to_reviews.rb
@@ -1,6 +1,6 @@
 class AddUserIdAndDestinationIdToReviews < ActiveRecord::Migration[6.0]
   def change
-    add_column: :reviews, :user_id, :integer
-    add_column: :reviews, :destination_id, :integer
+    add_column :reviews, :user_id, :integer
+    add_column :reviews, :destination_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_175557) do
+ActiveRecord::Schema.define(version: 2020_05_14_220627) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_05_13_175557) do
     t.integer "likes"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "destination_id"
   end
 
   create_table "users", force: :cascade do |t|
