d36849d9cfd5213dd577c25a7f32dc26089d4093
github username <github email address>
2020-05-14 18:09:28 -0400

    finishing up backend

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
 
-    belongs_to :user
-    belongs_to :destination
+    belongs_to :user, foreign_key: :user_id, class_name: "User"
+    belongs_to :destination, foreign_key: :destination_id, class_name: "Destination"
 end
