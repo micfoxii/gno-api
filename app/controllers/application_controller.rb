3ecf6027765b5430671285db921409da240b90d1
github username <github email address>
2020-05-18 01:00:52 -0400

    current user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+    include ::ActionController::Cookies
+
+    def current_user
+
+    end 
+    
+    def logged_in?
+        !!current_user
+    end
 end
