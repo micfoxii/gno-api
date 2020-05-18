788311b8910262bc90bf8e1880145876963fb479
github username <github email address>
2020-05-18 17:19:17 -0400

    server issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
 
     include ::ActionController::Cookies
 
-    def get_current_user
+    def current_user
         User.find_by(id: session[:user_id])
     end 
 
