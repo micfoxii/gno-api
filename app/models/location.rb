f2bd80796a68464c2fc7dffea402ac93e4817ac4
github username <github email address>
2020-05-26 11:17:43 -0400

    attributes and actions

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,7 @@ class Location < ApplicationRecord
 
     has_many :destinations, foreign_key: :location_id, class_name: "Destination"
     has_many :local_guides, foreign_key: :location_id, class_name: "User"
+    # has_many :reviews, foreign_key: :location_id, class_name: "Reviews"
 
     
 end
